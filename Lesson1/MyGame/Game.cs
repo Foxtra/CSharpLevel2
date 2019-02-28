d0f40e31a8fae95ad2e89a7fa7069210b19e6254
Foxtra <tkachew.sergey@ya.ru>
2019-02-28 22:39:26 +0300

    Добавлены далёкие звёзды в виде точек, ближние звёзды меняются в анимации, добавлены картинки для астероидов, планет

---
+++ Lesson1/MyGame/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        public static BaseObject[] _objs;
+
+        const int commonSpeed = 6;
+        const int planetSpeed = 3;
+        const int numOfPlanets = 3;
+        const int numOfStars = 30;
+        const int numOfAsteroids = 10;
+        const int numOfSmallStars = 50;
+        const int maxSize = 30;
+        const int minSize = 10;
+
+
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+        }
+
+        public static void Load()
+        {
+            Random rand = new Random();
+            _objs = new BaseObject[numOfStars + numOfAsteroids + numOfPlanets + numOfSmallStars];
+
+            for (int i = 0; i < _objs.Length - numOfStars - numOfPlanets - numOfAsteroids; i++)
+            {
+                _objs[i] = new SmallStar(new Point(Convert.ToInt32(rand.NextDouble() * (double)Game.Width),
+                    Convert.ToInt32(rand.NextDouble() * (double)Game.Height)), new Point(rand.Next(-commonSpeed, -1), 0), new Size(1, 1));
+            }
+
+            for (int i = _objs.Length - numOfStars - numOfPlanets - numOfAsteroids; i < _objs.Length - numOfPlanets - numOfAsteroids; i++)
+            {
+                int size = rand.Next(minSize, maxSize);
+                _objs[i] = new Star(new Point(Convert.ToInt32(rand.NextDouble() * (double)Game.Width),
+                    Convert.ToInt32(rand.NextDouble() * (double)Game.Height)), new Point(rand.Next(-commonSpeed * 2, -1), 0), new Size(size, size));
+            }
+
+            for (int i = _objs.Length  - numOfPlanets - numOfAsteroids; i < _objs.Length - numOfAsteroids; i++)
+            {
+                int size = rand.Next(minSize * 3, maxSize * 3);
+                _objs[i] = new Planet(new Point(Convert.ToInt32(rand.NextDouble() * (double)Game.Width),
+                    Convert.ToInt32(rand.NextDouble() * (double)Game.Height)), new Point(rand.Next(-planetSpeed, -1), 0), new Size(size, size));
+            }
+
+            for (int i = _objs.Length - numOfAsteroids; i < _objs.Length; i++)
+            {
+                int size = rand.Next(minSize, maxSize);
+                _objs[i] = new BaseObject(new Point(Convert.ToInt32(rand.NextDouble() * (double)(Game.Width - size)),
+                    Convert.ToInt32(rand.NextDouble() * (double)(Game.Height - size))), new Point(rand.Next(-commonSpeed, commonSpeed),
+                    rand.Next(-commonSpeed, commonSpeed)), new Size(size, size));
+            }
+        }
+
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            // Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+
+            Timer timer = new Timer { Interval = 50 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+
+    }
+}
+
---
+++ Lesson1/MyGame/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Planet : BaseObject
+    {
+        List<Bitmap> bitMapList = new List<Bitmap>() {
+            new Bitmap("..\\..\\img/giants/star_blue_giant01.png"),
+            new Bitmap("..\\..\\img/giants/star_red_giant01.png"), 
+            new Bitmap("..\\..\\img/giants/star_white_giant03.png") };
+
+        Bitmap image;
+        
+
+        public Planet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            image = bitMapList[random.Next(0,bitMapList.Count)];
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(image, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0 - Size.Width)
+            {
+                Pos.X = Game.Width + Size.Width;
+                image = bitMapList[random.Next(0, bitMapList.Count)];
+                Pos.Y = Convert.ToInt32(random.NextDouble() * (double)Game.Height);
+                Size.Width = Convert.ToInt32((random.NextDouble() + 0.5) * Size.Width);
+                Size.Height = Size.Width;
+                Dir.X = Convert.ToInt32((random.NextDouble() + 0.5) * Dir.X);
+            }
+        }
+    }
+}
