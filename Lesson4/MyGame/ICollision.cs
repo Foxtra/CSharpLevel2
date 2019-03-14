5cfd4a4307a7c5937677225dbdc3f8286de0b57b
Foxtra <tkachew.sergey@ya.ru>
2019-03-15 00:45:23 +0300

    Выполнил задания к уроку 4

---
+++ Lesson4/MyGame/ICollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace MyGame
+{
+    /// <summary>Определяет столкновения двух объектов</summary>
+    interface ICollision
+    {
+        bool Collision(ICollision obj);
+        Rectangle Rect { get; }
+
+    }
+}
