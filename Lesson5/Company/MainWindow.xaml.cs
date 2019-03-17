622c532a0ec467da6397572a7de9d97b18f714f1
Foxtra <tkachew.sergey@ya.ru>
2019-03-17 16:19:41 +0300

    выполнил задания

--- Lesson5/Company/MainWindow.xaml.cs
+++ Lesson5/Company/MainWindow.xaml.cs
@@ -10,8 +10,11 @@
  * Сергей Ткачев
  */
 
+using Company.Classes;
+using Company.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,9 +35,79 @@ namespace Company
     /// </summary>
     public partial class MainWindow : Window
     {
+        internal static DB db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new DB();
+            empList.ItemsSource = db.GetEmployees();
+            cbDepList.ItemsSource = db.GetDeptaments();
+            db.updateData += Update;
+        }
+
+        /// <summary>Обработка события выбора элемента из списка</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void Selected(object sender, SelectionChangedEventArgs args)
+        {
+            tbInfo.Text = db.GetInfo(sender);
+        }
+
+        /// <summary>Обработка нажатия кнопки "редактировать департамент"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnEditDep_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbDepList.SelectedItem != null)
+            {
+                DepEditWindow depEditWindow = new DepEditWindow(cbDepList.SelectedItem.ToString());
+                depEditWindow.Owner = this;
+                depEditWindow.Show();
+            }
+            else
+                MessageBox.Show("Выберете отдел для редактирования!");
+        }
+
+        /// <summary>Обработка нажатия кнопки "редактировать сотрудника"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnEditEmp_Click(object sender, RoutedEventArgs e)
+        {
+            if (empList.SelectedItem != null)
+            {
+                EmpEditWindow empEditWindow = new EmpEditWindow(empList.SelectedItem as Employee);
+                empEditWindow.Owner = this;
+                empEditWindow.Show();
+            }
+            else
+                MessageBox.Show("Выберете сотрудника для редактирования!");
+        }
+
+        /// <summary>Обработка нажатия кнопки "добавить сотрудника"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnCreateEmp_Click(object sender, RoutedEventArgs e)
+        {
+            AddEmpWindow addEmpWindow = new AddEmpWindow();
+            addEmpWindow.Owner = this;
+            addEmpWindow.Show();
+        }
+
+        /// <summary>Обработка нажатия кнопки "добавить департамент"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnCreateDep_Click(object sender, RoutedEventArgs e)
+        {
+            AddDepWindow addDepWindow = new AddDepWindow();
+            addDepWindow.Owner = this;
+            addDepWindow.Show();
+        }
+
+        /// <summary>Обновляет данные на форме</summary>
+        internal void Update()
+        {
+            empList.ItemsSource = db.GetEmployees();
+            cbDepList.ItemsSource = db.GetDeptaments();
         }
     }
 }
